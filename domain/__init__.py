e092597447447d7e6075bf3f57215badc04576bd
domospb <38582027+domospb@users.noreply.github.com>
2026-01-31 20:48:59 +0300

    Update domain package exports for clean imports.

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,3 +1,28 @@
 """
 Domain models for the WhoIsAlice ML service.
 """
+from .enums import DataType, TaskStatus, TransactionType, UserRole
+from .ml_model import MLModel
+from .ml_task import MLTask, PredictionResult
+from .transaction import CreditTransaction, DebitTransaction, Transaction
+from .user import AdminUser, RegularUser, User
+
+__all__ = [
+    # Enums
+    "UserRole",
+    "TaskStatus",
+    "TransactionType",
+    "DataType",
+    # User models
+    "User",
+    "RegularUser",
+    "AdminUser",
+    # Transaction models
+    "Transaction",
+    "CreditTransaction",
+    "DebitTransaction",
+    # ML models
+    "MLModel",
+    "MLTask",
+    "PredictionResult",
+]
